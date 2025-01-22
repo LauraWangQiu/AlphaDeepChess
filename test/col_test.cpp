489ad805325d00e54712bb3a1e20081666942ede
Juanvoid01 <jgironherranz@gmail.com>
2025-01-22 19:09:03 +0100

    row test refactored

--- test/col_test.cpp
+++ test/col_test.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include "test_utils.hpp"
 #include "col.hpp"
 
 static void col_to_char_test();
@@ -30,144 +30,145 @@ void col_test()
 
 static void col_to_char_test()
 {
-    std::cout << "col_to_char test :\n\n";
+    const std::string test_name = "col_to_char_test";
 
     for (Col i = COL_A; i <= COL_H; ++i) {
         if (col_to_char(i) != 'a' + i) {
-            std::cout << "TEST FAILED : col_to_char_test : col_to_char(i) != 'a' + i\n";
+            PRINT_TEST_FAILED(test_name, "col_to_char(i) != 'a' + i");
         }
     }
 }
 
 static void col_is_valid_col_test()
 {
-    std::cout << "col_is_valid_col_test test :\n\n";
+    const std::string test_name = "col_is_valid_col_test";
 
     for (Col i = COL_A; i <= COL_H; ++i) {
+
         if (!is_valid_col(i)) {
-            std::cout << "TEST FAILED : is_valid_col_test : !is_valid_col(i)\n";
+            PRINT_TEST_FAILED(test_name, "!is_valid_col(i)");
         }
     }
 
     if (is_valid_col(COL_INVALID)) {
-        std::cout << "TEST FAILED : is_valid_col_test : COL_INVALID considered valid\n";
+        PRINT_TEST_FAILED(test_name, "is_valid_col(COL_INVALID)");
     }
 }
 
 static void col_plus_test()
 {
-    std::cout << "col+ test :\n\n";
+    const std::string test_name = "col_plus_test";
 
     if (COL_A + 1 != COL_B) {
-        std::cout << "TEST FAILED : col_plus_test : COL_A + 1 != COL_B\n";
+        PRINT_TEST_FAILED(test_name, "COL_A + 1 != COL_B");
     }
 
     if (COL_H + 1 != COL_INVALID) {
-        std::cout << "TEST FAILED : col_plus_test : COL_H + 1 != COL_INVALID\n";
+        PRINT_TEST_FAILED(test_name, "COL_H + 1 != COL_INVALID");
     }
 }
 
 static void col_minus_test()
 {
-    std::cout << "col- test :\n\n";
+    const std::string test_name = "col_minus_test";
 
     if (COL_B - 1 != COL_A) {
-        std::cout << "TEST FAILED : col_minus_test : COL_B - 1 != COL_A\n";
+        PRINT_TEST_FAILED(test_name, "COL_B - 1 != COL_A");
     }
 
     if (COL_A - 1 != COL_INVALID) {
-        std::cout << "TEST FAILED : col_minus_test : COL_A - 1 != COL_INVALID\n";
+        PRINT_TEST_FAILED(test_name, "COL_A - 1 != COL_INVALID");
     }
 }
 
 static void col_pre_increment_test()
 {
-    std::cout << "pre-increment test :\n\n";
+    const std::string test_name = "col_pre_increment_test";
 
     Col col = COL_A;
     if (++col != COL_B) {
-        std::cout << "TEST FAILED : col_pre_increment_test : ++COL_A != COL_B\n";
+        PRINT_TEST_FAILED(test_name, "++COL_A != COL_B");
     }
 
     col = COL_H;
     if (++col != COL_INVALID) {
-        std::cout << "TEST FAILED : col_pre_increment_test : ++COL_H != COL_INVALID\n";
+        PRINT_TEST_FAILED(test_name, "++COL_H != COL_INVALID");
     }
 }
 
 static void col_post_increment_test()
 {
-    std::cout << "post-increment test :\n\n";
+    const std::string test_name = "col_post_increment_test";
 
     Col col = COL_A;
     if (col++ != COL_A || col != COL_B) {
-        std::cout << "TEST FAILED : col_post_increment_test : COL_A++ did not behave correctly\n";
+        PRINT_TEST_FAILED(test_name, "col++ != COL_A || col != COL_B");
     }
 
     col = COL_H;
     if (col++ != COL_H || col != COL_INVALID) {
-        std::cout << "TEST FAILED : col_post_increment_test : COL_H++ did not behave correctly\n";
+        PRINT_TEST_FAILED(test_name, "col++ != COL_H || col != COL_INVALID");
     }
 }
 
 static void col_pre_decrement_test()
 {
-    std::cout << "pre-decrement test :\n\n";
+    const std::string test_name = "col_pre_decrement_test";
 
     Col col = COL_B;
     if (--col != COL_A) {
-        std::cout << "TEST FAILED : col_pre_decrement_test : --COL_B != COL_A\n";
+        PRINT_TEST_FAILED(test_name, "--col != COL_A");
     }
 
     col = COL_A;
     if (--col != COL_INVALID) {
-        std::cout << "TEST FAILED : col_pre_decrement_test : --COL_A != COL_INVALID\n";
+        PRINT_TEST_FAILED(test_name, "--col != COL_INVALID");
     }
 }
 
 static void col_post_decrement_test()
 {
-    std::cout << "post-decrement test :\n\n";
+    const std::string test_name = "col_post_decrement_test";
 
     Col col = COL_B;
     if (col-- != COL_B || col != COL_A) {
-        std::cout << "TEST FAILED : col_post_decrement_test : COL_B-- did not behave correctly\n";
+        PRINT_TEST_FAILED(test_name, "col-- != COL_B || col != COL_A");
     }
 
     col = COL_A;
     if (col-- != COL_A || col != COL_INVALID) {
-        std::cout << "TEST FAILED : col_post_decrement_test : COL_A-- did not behave correctly\n";
+        PRINT_TEST_FAILED(test_name, "col-- != COL_A || col != COL_INVALID");
     }
 }
 
 static void col_plus_equals_test()
 {
-    std::cout << "col+= test :\n\n";
+    const std::string test_name = "col_plus_equals_test";
 
     Col col = COL_A;
     col += 1;
     if (col != COL_B) {
-        std::cout << "TEST FAILED : col_plus_equals_test : COL_A += 1 != COL_B\n";
+        PRINT_TEST_FAILED(test_name, "col != COL_B");
     }
 
     col += 7;
     if (col != COL_INVALID) {
-        std::cout << "TEST FAILED : col_plus_equals_test : COL_B += 7 != COL_INVALID\n";
+        PRINT_TEST_FAILED(test_name, "col != COL_INVALID");
     }
 }
 
 static void col_minus_equals_test()
 {
-    std::cout << "col-= test :\n\n";
+    const std::string test_name = "col_minus_equals_test";
 
     Col col = COL_B;
     col -= 1;
     if (col != COL_A) {
-        std::cout << "TEST FAILED : col_minus_equals_test : COL_B -= 1 != COL_A\n";
+        PRINT_TEST_FAILED(test_name, "col != COL_A");
     }
 
     col -= 1;
     if (col != COL_INVALID) {
-        std::cout << "TEST FAILED : col_minus_equals_test : COL_A -= 1 != COL_INVALID\n";
+        PRINT_TEST_FAILED(test_name, "col != COL_INVALID");
     }
 }
